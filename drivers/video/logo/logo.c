c3d6addb074cfd84171fb7dc4798a57276ec09f9
Krzysztof Chojnowski <krzysiek@embedev.pl>
2020-07-10 16:46:19 +0200

    SOMLABS: add 800x480 logo for kernel splash.

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -100,6 +100,10 @@ const struct linux_logo * __ref fb_find_logo(int depth)
 		/* SuperH Linux logo */
 		logo = &logo_superh_clut224;
 #endif
+#ifdef CONFIG_LOGO_SOMLABS_800_480_CLUT224
+		/* SoMLabs Linux 800x480 logo */
+		logo = &logo_somlabs_800_480_clut224;
+#endif
 	}
 	return logo;
 }
