01c5f19c5db90ee78628783635081f4ca812224e
Krzysztof Chojnowski <krzysztof.chojnowski@somlabs.com>
2022-12-02 12:17:08 +0100

    SOMLABS: fixed display configuration names and models

--- drivers/gpu/drm/panel/panel-riverdi-rvt70hsmnwc00.c
+++ drivers/gpu/drm/panel/panel-riverdi-rvt70hsmnwc00.c
@@ -15,7 +15,7 @@
 #include <drm/drm_modes.h>
 #include <drm/drm_panel.h>
 
-struct rvt70hsmncw00 {
+struct rvt70hsmnwc00 {
 	struct device *dev;
 	struct drm_panel panel;
 	struct gpio_desc *reset_gpio;
@@ -23,7 +23,7 @@ struct rvt70hsmncw00 {
 	bool enabled;
 };
 
-static const struct drm_display_mode rvt70hsmncw00_default_mode = {
+static const struct drm_display_mode rvt70hsmnwc00_default_mode = {
         .clock       = 51000,
         .hdisplay    = 1024,
         .hsync_start = 1024 + 160,
@@ -38,12 +38,12 @@ static const struct drm_display_mode rvt70hsmncw00_default_mode = {
         .height_mm   = 122,
 };
 
-static inline struct rvt70hsmncw00 *panel_to_rvt70hsmncw00(struct drm_panel *panel)
+static inline struct rvt70hsmnwc00 *panel_to_rvt70hsmnwc00(struct drm_panel *panel)
 {
-	return container_of(panel, struct rvt70hsmncw00, panel);
+	return container_of(panel, struct rvt70hsmnwc00, panel);
 }
 
-static void rvt70hsmncw00_dcs_write_cmd(struct rvt70hsmncw00 *ctx, u8 cmd)
+static void rvt70hsmnwc00_dcs_write_cmd(struct rvt70hsmnwc00 *ctx, u8 cmd)
 {
 	struct mipi_dsi_device *dsi = to_mipi_dsi_device(ctx->dev);
 	int err;
@@ -53,7 +53,7 @@ static void rvt70hsmncw00_dcs_write_cmd(struct rvt70hsmncw00 *ctx, u8 cmd)
                 dev_err_ratelimited(ctx->dev, "write failed: %d\n", err);
 }
 
-static void rvt70hsmncw00_dcs_write_cmd_with_param(struct rvt70hsmncw00 *ctx, u8 cmd, u8 value)
+static void rvt70hsmnwc00_dcs_write_cmd_with_param(struct rvt70hsmnwc00 *ctx, u8 cmd, u8 value)
 {
         struct mipi_dsi_device *dsi = to_mipi_dsi_device(ctx->dev);
         int err;
@@ -66,31 +66,31 @@ static void rvt70hsmncw00_dcs_write_cmd_with_param(struct rvt70hsmncw00 *ctx, u8
                 dev_err_ratelimited(ctx->dev, "write failed: %d\n", err);
 }
 
-static void rvt70hsmncw00_init_sequence(struct rvt70hsmncw00 *ctx)
+static void rvt70hsmnwc00_init_sequence(struct rvt70hsmnwc00 *ctx)
 {
-        rvt70hsmncw00_dcs_write_cmd(ctx, 0x01);
+        rvt70hsmnwc00_dcs_write_cmd(ctx, 0x01);
         msleep(120);
 
-        rvt70hsmncw00_dcs_write_cmd_with_param(ctx, 0xB2, 0x70);
-        rvt70hsmncw00_dcs_write_cmd_with_param(ctx, 0x80, 0x4B);
-        rvt70hsmncw00_dcs_write_cmd_with_param(ctx, 0x81, 0xFF);
-        rvt70hsmncw00_dcs_write_cmd_with_param(ctx, 0x82, 0x1A);
-        rvt70hsmncw00_dcs_write_cmd_with_param(ctx, 0x83, 0x88);
-        rvt70hsmncw00_dcs_write_cmd_with_param(ctx, 0x84, 0x8F);
-        rvt70hsmncw00_dcs_write_cmd_with_param(ctx, 0x85, 0x35);
-        rvt70hsmncw00_dcs_write_cmd_with_param(ctx, 0x86, 0xB0);
+        rvt70hsmnwc00_dcs_write_cmd_with_param(ctx, 0xB2, 0x70);
+        rvt70hsmnwc00_dcs_write_cmd_with_param(ctx, 0x80, 0x4B);
+        rvt70hsmnwc00_dcs_write_cmd_with_param(ctx, 0x81, 0xFF);
+        rvt70hsmnwc00_dcs_write_cmd_with_param(ctx, 0x82, 0x1A);
+        rvt70hsmnwc00_dcs_write_cmd_with_param(ctx, 0x83, 0x88);
+        rvt70hsmnwc00_dcs_write_cmd_with_param(ctx, 0x84, 0x8F);
+        rvt70hsmnwc00_dcs_write_cmd_with_param(ctx, 0x85, 0x35);
+        rvt70hsmnwc00_dcs_write_cmd_with_param(ctx, 0x86, 0xB0);
 }
 
-static int rvt70hsmncw00_enable(struct drm_panel *panel)
+static int rvt70hsmnwc00_enable(struct drm_panel *panel)
 {
-        struct rvt70hsmncw00 *ctx = panel_to_rvt70hsmncw00(panel);
+        struct rvt70hsmnwc00 *ctx = panel_to_rvt70hsmnwc00(panel);
         struct mipi_dsi_device *dsi = to_mipi_dsi_device(ctx->dev);
         int ret;
 
         if (ctx->enabled)
                 return 0;
 
-        rvt70hsmncw00_init_sequence(ctx);
+        rvt70hsmnwc00_init_sequence(ctx);
 
         ret = mipi_dsi_dcs_exit_sleep_mode(dsi);
         if (ret)
@@ -109,9 +109,9 @@ static int rvt70hsmncw00_enable(struct drm_panel *panel)
         return 0;
 }
 
-static int rvt70hsmncw00_disable(struct drm_panel *panel)
+static int rvt70hsmnwc00_disable(struct drm_panel *panel)
 {
-	struct rvt70hsmncw00 *ctx = panel_to_rvt70hsmncw00(panel);
+	struct rvt70hsmnwc00 *ctx = panel_to_rvt70hsmnwc00(panel);
 	struct mipi_dsi_device *dsi = to_mipi_dsi_device(ctx->dev);
 	int ret;
 
@@ -133,9 +133,9 @@ static int rvt70hsmncw00_disable(struct drm_panel *panel)
 	return 0;
 }
 
-static int rvt70hsmncw00_unprepare(struct drm_panel *panel)
+static int rvt70hsmnwc00_unprepare(struct drm_panel *panel)
 {
-	struct rvt70hsmncw00 *ctx = panel_to_rvt70hsmncw00(panel);
+	struct rvt70hsmnwc00 *ctx = panel_to_rvt70hsmnwc00(panel);
 
 	if (!ctx->prepared)
 		return 0;
@@ -151,9 +151,9 @@ static int rvt70hsmncw00_unprepare(struct drm_panel *panel)
 	return 0;
 }
 
-static int rvt70hsmncw00_prepare(struct drm_panel *panel)
+static int rvt70hsmnwc00_prepare(struct drm_panel *panel)
 {
-	struct rvt70hsmncw00 *ctx = panel_to_rvt70hsmncw00(panel);
+	struct rvt70hsmnwc00 *ctx = panel_to_rvt70hsmnwc00(panel);
 
 	if (ctx->prepared)
 		return 0;
@@ -171,17 +171,17 @@ static int rvt70hsmncw00_prepare(struct drm_panel *panel)
 	return 0;
 }
 
-static int rvt70hsmncw00_get_modes(struct drm_panel *panel,
+static int rvt70hsmnwc00_get_modes(struct drm_panel *panel,
 			     struct drm_connector *connector)
 {
 	struct drm_display_mode *mode;
 
-	mode = drm_mode_duplicate(connector->dev, &rvt70hsmncw00_default_mode);
+	mode = drm_mode_duplicate(connector->dev, &rvt70hsmnwc00_default_mode);
 	if (!mode) {
 		dev_err(panel->dev, "failed to add mode %ux%u@%u\n",
-			rvt70hsmncw00_default_mode.hdisplay,
-			rvt70hsmncw00_default_mode.vdisplay,
-			drm_mode_vrefresh(&rvt70hsmncw00_default_mode));
+			rvt70hsmnwc00_default_mode.hdisplay,
+			rvt70hsmnwc00_default_mode.vdisplay,
+			drm_mode_vrefresh(&rvt70hsmnwc00_default_mode));
 		return -ENOMEM;
 	}
 
@@ -196,18 +196,18 @@ static int rvt70hsmncw00_get_modes(struct drm_panel *panel,
 	return 1;
 }
 
-static const struct drm_panel_funcs rvt70hsmncw00_drm_funcs = {
-	.disable = rvt70hsmncw00_disable,
-	.unprepare = rvt70hsmncw00_unprepare,
-	.prepare = rvt70hsmncw00_prepare,
-	.enable = rvt70hsmncw00_enable,
-	.get_modes = rvt70hsmncw00_get_modes,
+static const struct drm_panel_funcs rvt70hsmnwc00_drm_funcs = {
+	.disable = rvt70hsmnwc00_disable,
+	.unprepare = rvt70hsmnwc00_unprepare,
+	.prepare = rvt70hsmnwc00_prepare,
+	.enable = rvt70hsmnwc00_enable,
+	.get_modes = rvt70hsmnwc00_get_modes,
 };
 
-static int rvt70hsmncw00_probe(struct mipi_dsi_device *dsi)
+static int rvt70hsmnwc00_probe(struct mipi_dsi_device *dsi)
 {
 	struct device *dev = &dsi->dev;
-	struct rvt70hsmncw00 *ctx;
+	struct rvt70hsmnwc00 *ctx;
 	int ret;
 
 	ctx = devm_kzalloc(dev, sizeof(*ctx), GFP_KERNEL);
@@ -230,7 +230,7 @@ static int rvt70hsmncw00_probe(struct mipi_dsi_device *dsi)
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE | MIPI_DSI_MODE_LPM;
 
-	drm_panel_init(&ctx->panel, dev, &rvt70hsmncw00_drm_funcs,
+	drm_panel_init(&ctx->panel, dev, &rvt70hsmnwc00_drm_funcs,
 		       DRM_MODE_CONNECTOR_DSI);
 
 	ret = drm_panel_of_backlight(&ctx->panel);
@@ -249,9 +249,9 @@ static int rvt70hsmncw00_probe(struct mipi_dsi_device *dsi)
 	return 0;
 }
 
-static int rvt70hsmncw00_remove(struct mipi_dsi_device *dsi)
+static int rvt70hsmnwc00_remove(struct mipi_dsi_device *dsi)
 {
-	struct rvt70hsmncw00 *ctx = mipi_dsi_get_drvdata(dsi);
+	struct rvt70hsmnwc00 *ctx = mipi_dsi_get_drvdata(dsi);
 	struct device *dev = &dsi->dev;
 
 	mipi_dsi_detach(dsi);
@@ -260,21 +260,21 @@ static int rvt70hsmncw00_remove(struct mipi_dsi_device *dsi)
 	return 0;
 }
 
-static const struct of_device_id riverdi_rvt70hsmncw00_of_match[] = {
-	{ .compatible = "riverdi,rvt70hsmncw00" },
+static const struct of_device_id riverdi_rvt70hsmnwc00_of_match[] = {
+	{ .compatible = "riverdi,rvt70hsmnwc00" },
 	{ }
 };
-MODULE_DEVICE_TABLE(of, riverdi_rvt70hsmncw00_of_match);
+MODULE_DEVICE_TABLE(of, riverdi_rvt70hsmnwc00_of_match);
 
-static struct mipi_dsi_driver riverdi_rvt70hsmncw00_driver = {
-	.probe = rvt70hsmncw00_probe,
-	.remove = rvt70hsmncw00_remove,
+static struct mipi_dsi_driver riverdi_rvt70hsmnwc00_driver = {
+	.probe = rvt70hsmnwc00_probe,
+	.remove = rvt70hsmnwc00_remove,
 	.driver = {
-		.name = "panel-riverdi-rvt70hsmncw00",
-		.of_match_table = riverdi_rvt70hsmncw00_of_match,
+		.name = "panel-riverdi-rvt70hsmnwc00",
+		.of_match_table = riverdi_rvt70hsmnwc00_of_match,
 	},
 };
-module_mipi_dsi_driver(riverdi_rvt70hsmncw00_driver);
+module_mipi_dsi_driver(riverdi_rvt70hsmnwc00_driver);
 
 MODULE_AUTHOR("Krzysztof Chojnowski <krzysztof.chojnowski@somlabs.com>");
 MODULE_DESCRIPTION("DRM Driver for riverdi RVT70HSMNWC00 MIPI DSI panel");
