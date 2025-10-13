79decb8612ec9163746203b7d23910c685c52e4c
Krzysztof Chojnowski <krzysztof.chojnowski@somlabs.com>
2025-10-13 23:26:35 +0200

    SOMLABS: add clk/data polarity swap option in imx-mipi-csis.c driver

--- drivers/media/platform/nxp/imx-mipi-csis.c
+++ drivers/media/platform/nxp/imx-mipi-csis.c
@@ -22,6 +22,7 @@
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/of.h>
+#include <linux/of_graph.h>
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
 #include <linux/regulator/consumer.h>
@@ -350,6 +351,8 @@ struct mipi_csis_device {
 	u32 clk_frequency;
 	u32 hs_settle;
 	u32 clk_settle;
+	u8 swap_clk;
+	u8 swap_data;
 
 	spinlock_t slock;	/* Protect events */
 	struct mipi_csis_event events[MIPI_CSIS_NUM_EVENTS];
@@ -670,6 +673,21 @@ static int mipi_csis_calculate_params(struct mipi_csis_device *csis,
 	return 0;
 }
 
+static void mipi_csis_set_set_data_clock_polarity(struct mipi_csis_device *csis)
+{
+        u32 val = mipi_csis_read(csis, MIPI_CSIS_DPHY_CMN_CTRL);
+
+        val &= ~(MIPI_CSIS_DPHY_CMN_CTRL_DPDN_SWAP_CLK | MIPI_CSIS_DPHY_CMN_CTRL_DPDN_SWAP_DAT);
+
+        if(csis->swap_clk)
+                val |= MIPI_CSIS_DPHY_CMN_CTRL_DPDN_SWAP_CLK;
+
+        if(csis->swap_data)
+                val |= MIPI_CSIS_DPHY_CMN_CTRL_DPDN_SWAP_DAT;
+
+        mipi_csis_write(csis, MIPI_CSIS_DPHY_CMN_CTRL, val);
+}
+
 static void mipi_csis_set_params(struct mipi_csis_device *csis,
 				 const struct v4l2_mbus_framefmt *format,
 				 const struct csis_pix_format *csis_fmt)
@@ -689,6 +707,8 @@ static void mipi_csis_set_params(struct mipi_csis_device *csis,
 	mipi_csis_write(csis, MIPI_CSIS_DPHY_CMN_CTRL,
 			MIPI_CSIS_DPHY_CMN_CTRL_HSSETTLE(csis->hs_settle) |
 			MIPI_CSIS_DPHY_CMN_CTRL_CLKSETTLE(csis->clk_settle));
+
+	mipi_csis_set_set_data_clock_polarity(csis);
 
 	mipi_csis_write(csis, MIPI_CSIS_ISP_SYNC_CH(0),
 			MIPI_CSIS_ISP_SYNC_HSYNC_LINTV(0) |
@@ -1493,6 +1513,19 @@ static int mipi_csis_parse_dt(struct mipi_csis_device *csis)
 	if (csis->num_channels < 1 || csis->num_channels > MIPI_CSIS_MAX_CHANNELS)
 		return dev_err_probe(csis->dev, -EINVAL,
 				     "Invalid fsl,num-channels value\n");
+
+	node = of_graph_get_next_endpoint(node, NULL);
+	if (!node) {
+		dev_err(csis->dev, "No port node at %s\n",
+			csis->dev->of_node->full_name);
+		return -EINVAL;
+	}
+
+	csis->swap_data = of_property_read_bool(node,
+						"swap-data");
+
+	csis->swap_clk = of_property_read_bool(node,
+						"swap-clk");
 
 	return 0;
 }
